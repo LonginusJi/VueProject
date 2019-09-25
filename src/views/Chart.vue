e5f95c0f9b3bb200e13b7aee3e3323e7a52971e8
yijunji <jiyijun@outlook.com>
2019-09-25 16:17:29 +0800

    add ForceChart

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -2,8 +2,12 @@
   <div class="charts">
     <v-tabs fixed-tabs background-color="indigo" dark grow>
       <v-tab>Force Chart</v-tab>
+      <v-tab>Divided Force</v-tab>
       <v-tab>Bar Chart</v-tab>
       <v-tab>Circle Chart</v-tab>
+      <v-tab-item>
+        <ForceChart></ForceChart>
+      </v-tab-item>
       <v-tab-item>
         <ForceBar></ForceBar>
       </v-tab-item>
@@ -19,12 +23,12 @@
 
 <script>
 import HighChart from "../components/HighChart.vue";
-// import ForceChart from "../components/ForceChart.vue";
+import ForceChart from "../components/ForceChart.vue";
 import CircleChart from "../components/CircleChart.vue";
 import ForceBar from "../components/ForceBar.vue";
 export default {
   components: {
-    // ForceChart,
+    ForceChart,
     CircleChart,
     HighChart,
     ForceBar
