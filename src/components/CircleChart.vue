3e5114d722680b12f4c6d33664f9220c2207dca3
yijunji <jiyijun@outlook.com>
2019-09-26 16:18:30 +0800

    Update setting

--- src/components/CircleChart.vue
+++ src/components/CircleChart.vue
@@ -14,6 +14,10 @@ export default {
       d3.csv(
         "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv"
       ).then(data => {
+        data.forEach(d => {
+          d.gdpPercap = +d.gdpPercap;
+          d.pop = +d.pop;
+        });
         new CircleChart(data);
       });
     }
@@ -25,7 +29,7 @@ export default {
 svg .bubbles {
   stroke-width: 1px;
   stroke: black;
-  opacity: 0.8;
+  /* opacity: 0.8; */
 }
 
 .bubbles:hover {
