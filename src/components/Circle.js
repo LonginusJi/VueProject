592b98aee119d695cef4777bd1f9d9febb4abd8e
yijunji <jiyijun@outlook.com>
2019-09-26 15:43:03 +0800

    Optimezi code

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -2,99 +2,54 @@ import * as d3 from "d3";
 export default class CircleChart {
     constructor(data) {
         // set the dimensions and margins of the graph
-        var margin = {
-            top: 40,
-            right: 150,
-            bottom: 60,
-            left: 30
-        },
-            width = 500 - margin.left - margin.right,
-            height = 420 - margin.top - margin.bottom;
-
+        let margin = { top: 40, right: 150, bottom: 60, left: 30 };
+        this.width = 600 - margin.left - margin.right;
+        this.height = 500 - margin.top - margin.bottom;
         // append the svg object to the body of the page
-        var svg = d3
+        this.svg = d3
             .select("#CircleChart")
             .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-
+            .attr("width", this.width + margin.left + margin.right)
+            .attr("height", this.height + margin.top + margin.bottom)
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        this.initdata(data)
+        this.initLegend_pop()
+        this.initLegend_contry()
+        this.initTooltip()
+        this.plotLinks()
+        this.plotCircles()
+    }
 
-        //Read the data
-
+    initdata(data) {
         data.forEach(d => {
             d.gdpPercap = +d.gdpPercap;
             d.pop = +d.pop;
         });
-        data.sort(function (a, b) {
-            return d3.descending(a.pop, b.pop); //DEscending
-        });
-        var newData = [];
-        for (var i = 0; i <= 9; i++) {
-            newData.push(data[i]);
-        }
-        data = newData;
-        // ---------------------------//
-        //       AXIS  AND SCALE      //
-        // ---------------------------//
-
-        // Add X axis
-        var x = d3
-            .scaleLinear()
-            .domain([0, 45000])
-            .range([0, width]);
-        // svg
-        //   .append("g")
-        //   // .attr("transform", "translate(0," + height + ")")
-        //   .attr("transform", "translate(0,1000)")
-        //   .call(d3.axisBottom(x).ticks(3));
-
-        // // Add X axis label:
-        // svg
-        //   .append("text")
-        //   .attr("text-anchor", "end")
-        //   .attr("x", width)
-        //   .attr("y", height + 50);
-        // // F;.text("Gdp per Capita");
-
-        // Add Y axis
-        var y = d3
-            .scaleLinear()
-            .domain([35, 90])
-            .range([height, 0]);
-
-        // svg
-        //   .append("g")
-        //   .call(d3.axisLeft(y))
-        //   .attr("transform", "translate(0,1000)");
-
-        // // Add Y axis label:
-        // svg
-        //   .append("text")
-        //   .attr("text-anchor", "end")
-        //   .attr("x", 0)
-        //   .attr("y", -20)
-        //   // .text("Life expectancy")
-        //   .attr("text-anchor", "start");
+        data.sort((a, b) => (d3.descending(a.pop, b.pop)));//DEscending 
+        let newData = [];
+        for (let i = 0; i <= 9; i++) { newData.push(data[i]); }
+        this.data = newData;
+        let angleNum = this.data.length - 1;
+        this.angle = (2 * Math.PI) / angleNum;
+        this.linkData = this.data.slice(1);
 
         // Add a scale for bubble size
-        var z = d3
+        this.radius = d3
             .scaleSqrt()
             .domain([200000, 1310000000])
             .range([2, 30]);
-
         // Add a scale for bubble color
-        var myColor = d3
+        this.myColor = d3
             .scaleOrdinal()
             .domain(["Asia", "Europe", "Americas", "Africa", "Oceania"])
             .range(d3.schemeSet1);
-
-        // ---------------------------//
-        //      TOOLTIP               //
-        // ---------------------------//
-
+    }
+    // ---------------------------//
+    //      TOOLTIP               //
+    // ---------------------------//
+    initTooltip() {
         // -1- Create a tooltip div that is hidden by default:
-        var tooltip = d3
+        let tooltip = d3
             .select("#CircleChart")
             .append("div")
             .style("opacity", 0)
@@ -105,209 +60,167 @@ export default class CircleChart {
             .style("color", "white");
 
         // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
-        var showTooltip = function (d) {
-            tooltip.transition().duration(200);
+        this.showTooltip = d => (
             tooltip
                 .style("opacity", 1)
-                .html("Country: " + d.country)
-                .style("left", d3.mouse(this)[0] + 30 + "px")
-                .style("top", d3.mouse(this)[1] + 30 + "px");
-        };
-        var moveTooltip = function (d) {
-            tooltip
-                .style("left", d3.mouse(this)[0] + 30 + "px")
-                .style("top", d3.mouse(this)[1] + 30 + "px");
-        };
-        var hideTooltip = function (d) {
-            tooltip
-                .transition()
-                .duration(200)
-                .style("opacity", 0);
-        };
+                .html("Country: " + d.country + ", Continent: " + d.continent + ", Popilation: " + d.pop / 1000000 + "M")
+            // .style("left", d3.mouse(this)[0] + 30 + "px")
+            // .style("top", d3.mouse(this)[1] + 30 + "px");
+        );
+        // let moveTooltip = function (d) {
+        //     tooltip
+        //         .style("left", d3.mouse(this)[0] + 30 + "px")
+        //         .style("top", d3.mouse(this)[1] + 30 + "px");
+        // };
+        this.hideTooltip = () => (
+            tooltip.style("opacity", 0)
+        );
+    }
+    // ---------------------------//    
+    //       LEGEND              //
+    // ---------------------------//
+    initLegend_pop() {
+        // Add legend: circles
+        let valuesToShow = [10000000, 100000000, 1000000000];
+        let xCircle = 490;
+        let xLabel = 540;
+        const legend_pop = this.svg
+            .append('g')
+            .attr('class', 'legend_pop')
 
+        legend_pop.selectAll("legend")
+            .data(valuesToShow)
+            .join("circle")
+            .attr("cx", xCircle)
+            .attr("cy", d => (this.height - 150 - this.radius(d)))
+            .attr("r", d => this.radius(d))
+            .style("fill", "none")
+            .attr("stroke", "black");
+
+        // Add legend: segments
+        legend_pop
+            .selectAll("legend")
+            .data(valuesToShow)
+            .join("line")
+            .attr("x1", d => (xCircle + this.radius(d)))
+            .attr("x2", xLabel)
+            .attr("y1", d => (this.height - 150 - this.radius(d)))
+            .attr("y2", d => (this.height - 150 - this.radius(d)))
+            .attr("stroke", "black")
+            .style("stroke-dasharray", "2,2");
+
+        // Add legend: labels
+        legend_pop
+            .selectAll("legend")
+            .data(valuesToShow)
+            .join("text")
+            .attr("x", xLabel)
+            .attr("y", d => (this.height - 150 - this.radius(d)))
+            .text(d => d / 1000000)
+            .style("font-size", 10)
+            .attr("alignment-baseline", "middle");
+
+        // Legend title
+        legend_pop
+            .append("text")
+            .attr("x", xCircle)
+            .attr("y", this.height - 150 + 30)
+            .text("Population (M)")
+            .style('font-size', 14)
+            .attr("text-anchor", "middle");
+    }
+    initLegend_contry() {
         // ---------------------------//
         //       HIGHLIGHT GROUP      //
         // ---------------------------//
 
         // What to do when one group is hovered
-        var highlight = function (d) {
+        let highlight = d => {
             // reduce opacity of all groups
             d3.selectAll(".bubbles").style("opacity", 0.05);
             // expect the one that is hovered
             d3.selectAll("." + d).style("opacity", 1);
         };
-
         // And when it is not hovered anymore
-        var noHighlight = function (d) {
+        let noHighlight = () => {
             d3.selectAll(".bubbles").style("opacity", 1);
         };
 
-        // ---------------------------//
-        //       CIRCLES              //
-        // ---------------------------//
+        // Add one dot in the legend for each name
+        let distance = 25;
+        let allgroups = ["Asia", "Europe", "Americas", "Africa", "Oceania"];
 
-        // Add dots
-        var angleNum = data.length - 1;
-        var angle = (2 * Math.PI) / angleNum;
-        svg
-            .append("g")
-            .selectAll("dot")
-            .data(data)
-            .join("circle")
-            .attr("id", d => d.country)
-            .attr("class", function (d) {
-                return "bubbles " + d.continent;
-            })
-            .attr("cx", function (d, i) {
-                return Math.cos(angle * i) * 10 * z(d.pop) + width / 2;
-            })
-            .attr("cy", function (d, i) {
-                return Math.sin(angle * i) * 10 * z(d.pop) + height / 2;
-            })
-            .attr("r", function (d) {
-                return z(d.pop);
-            })
-            .style("fill", function (d) {
-                return myColor(d.continent);
-            })
+        const legend_country = this.svg.append('g')
+            .attr('class', 'legend_country')
 
-            // -3- Trigger the functions for hover
-            .on("mouseover", showTooltip)
-            .on("mousemove", moveTooltip)
-            .on("mouseleave", hideTooltip)
-            .selectAll("circle")
-            // .distance(d => d.pop * 20)
-            .attr("transform", function (d, i) {
-                return "translate(0," + 550 * i + ")";
-            });
-
-        data.splice(0, 1);
-
-        var link = svg
-            .append("g")
-            .attr("class", "links")
-            .selectAll(".links")
-            .data(data)
-            .enter()
-            .append("line")
-            .attr("x1", height / 2)
-            .attr("y1", width / 2)
-            .attr("x2", function (d, i) {
-                return Math.cos(angle * (i + 1)) * 9 * z(d.pop) + width / 2;
-            })
-            .attr("y2", function (d, i) {
-                return Math.sin(angle * (i + 1)) * 9 * z(d.pop) + height / 2;
-            });
-
-        d3.select("#China")
-            .attr("cx", width / 2)
-            .attr("cy", height / 2);
-
-        console.log(data);
-        // ---------------------------//
-        //       LEGEND              //
-        // ---------------------------//
-
-        // Add legend: circles
-        var valuesToShow = [10000000, 100000000, 1000000000];
-        var xCircle = 390;
-        var xLabel = 440;
-        svg
-            .selectAll("legend")
-            .data(valuesToShow)
-            .enter()
-            .append("circle")
-            .attr("cx", xCircle)
-            .attr("cy", function (d) {
-                return height - 100 - z(d);
-            })
-            .attr("r", function (d) {
-                return z(d);
-            })
-            .style("fill", "none")
-            .attr("stroke", "black");
-
-        // Add legend: segments
-        svg
-            .selectAll("legend")
-            .data(valuesToShow)
-            .enter()
-            .append("line")
-            .attr("x1", function (d) {
-                return xCircle + z(d);
-            })
-            .attr("x2", xLabel)
-            .attr("y1", function (d) {
-                return height - 100 - z(d);
-            })
-            .attr("y2", function (d) {
-                return height - 100 - z(d);
-            })
-            .attr("stroke", "black")
-            .style("stroke-dasharray", "2,2");
-
-        // Add legend: labels
-        svg
-            .selectAll("legend")
-            .data(valuesToShow)
-            .enter()
-            .append("text")
-            .attr("x", xLabel)
-            .attr("y", function (d) {
-                return height - 100 - z(d);
-            })
-            .text(function (d) {
-                return d / 1000000;
-            })
-            .style("font-size", 10)
-            .attr("alignment-baseline", "middle");
-
-        // Legend title
-        svg
-            .append("text")
-            .attr("x", xCircle)
-            .attr("y", height - 100 + 30)
-            .text("Population (M)")
-            .attr("text-anchor", "middle");
-
-        // Add one dot in the legend for each name.
-        var size = 20;
-        var allgroups = ["Asia", "Europe", "Americas", "Africa", "Oceania"];
-        svg
-            .selectAll("myrect")
+        legend_country.selectAll("myrect")
             .data(allgroups)
-            .enter()
-            .append("circle")
-            .attr("cx", 390)
-            .attr("cy", function (d, i) {
-                return 10 + i * (size + 5);
-            }) // 100 is where the first dot appears. 25 is the distance between dots
+            .join("circle")
+            .attr("cx", 490)
+            .attr("cy", (d, i) => (20 + i * (distance + 5))) // 100 is where the first dot appears. 25 is the distance between dots
             .attr("r", 7)
-            .style("fill", function (d) {
-                return myColor(d);
-            })
+            .style("fill", d => this.myColor(d))
             .on("mouseover", highlight)
             .on("mouseleave", noHighlight);
 
         // Add labels beside legend dots
-        svg
+        legend_country
             .selectAll("mylabels")
             .data(allgroups)
-            .enter()
-            .append("text")
-            .attr("x", 390 + size * 0.8)
-            .attr("y", function (d, i) {
-                return i * (size + 5) + size / 2;
-            }) // 100 is where the first dot appears. 25 is the distance between dots
-            .style("fill", function (d) {
-                return myColor(d);
-            })
-            .text(function (d) {
-                return d;
-            })
+            .join("text")
+            .attr("x", 490 + distance * 0.8)
+            .attr("y", (d, i) => (i * (distance + 5) + distance / 2 + 10)) // 100 is where the first dot appears. 25 is the distance between dots
+            .style("fill", d => this.myColor(d))
+            .text(d => d)
             .attr("text-anchor", "left")
+            .style('font-size', 14)
             .style("alignment-baseline", "middle")
             .on("mouseover", highlight)
             .on("mouseleave", noHighlight);
+    }
+    // ---------------------------//
+    //       CIRCLES              //
+    // ---------------------------//
+    plotCircles() {
+        // Add circles
+        this.svg
+            .append("g")
+            .attr("class", "circles")
+            .selectAll("circle")
+            .data(this.data)
+            .join("circle")
+            .attr("id", d => d.country)
+            .attr("class", d => ("bubbles " + d.continent))
+            .attr("cx", (d, i) => (Math.cos(this.angle * i) * 10 * this.radius(d.pop) + this.width / 2))
+            .attr("cy", (d, i) => (Math.sin(this.angle * i) * 10 * this.radius(d.pop) + this.height / 2))
+            .attr("r", d => this.radius(d.pop))
+            .style("fill", d => this.myColor(d.continent))
+            // -3- Trigger the functions for hover
+            .on("mouseover", this.showTooltip)
+            // .on("mousemove", moveTooltip)
+            .on("mouseleave", this.hideTooltip)
+            .selectAll("circle")
+            // .distance(d => d.pop * 20)
+            .attr("transform", (d, i) => ("translate(0," + 550 * i + ")"));
+
+        d3.select("#China")
+            .attr("cx", this.width / 2)
+            .attr("cy", this.height / 2);
+    }
+    // ---------------------------//
+    //         LINKS              //
+    // ---------------------------//
+    plotLinks() {
+        this.svg
+            .append("g")
+            .attr("class", "links")
+            .selectAll(".links")
+            .data(this.linkData)
+            .join("line")
+            .attr("x1", this.width / 2)
+            .attr("y1", this.height / 2)
+            .attr("x2", (d, i) => (Math.cos(this.angle * (i + 1)) * 9 * this.radius(d.pop) + this.width / 2))
+            .attr("y2", (d, i) => (Math.sin(this.angle * (i + 1)) * 9 * this.radius(d.pop) + this.height / 2))
+            .attr('stroke-dasharray', ('5, 3'));
     }
 }
--- src/components/CircleChart.vue
+++ src/components/CircleChart.vue
@@ -11,9 +11,6 @@ export default {
   },
   methods: {
     initchart() {
-      // let data = require("../data/netw_lv_1_kmeans_4_Clust_4fe.json");
-      // let width = 960;
-      // let height = 450;
       d3.csv(
         "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv"
       ).then(data => {
