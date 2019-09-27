5bb9581f1658e10a8e2dc9ea535e2be906d717e1
yijunji <jiyijun@outlook.com>
2019-09-27 15:55:37 +0800

    Optimize code

--- src/components/ForceChart.vue
+++ src/components/ForceChart.vue
@@ -16,12 +16,7 @@ export default {
 
     var simulation = d3
       .forceSimulation()
-      .force(
-        "link",
-        d3.forceLink().id(function(d) {
-          return d.id;
-        })
-      )
+      .force("link", d3.forceLink().id(d => d.id))
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(width / 2, height / 2));
 
@@ -36,9 +31,7 @@ export default {
       .attr("stroke", "#999")
       .attr("stroke-opacity", 0.6)
       // .attr('opacity',0)
-      .attr("stroke-width", function(d) {
-        return Math.sqrt(d.value);
-      });
+      .attr("stroke-width", d => Math.sqrt(d.value));
 
     var node = svg
       .append("g")
@@ -80,22 +73,12 @@ export default {
 
     function ticked() {
       link
-        .attr("x1", function(d) {
-          return d.source.x;
-        })
-        .attr("y1", function(d) {
-          return d.source.y;
-        })
-        .attr("x2", function(d) {
-          return d.target.x;
-        })
-        .attr("y2", function(d) {
-          return d.target.y;
-        });
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
 
-      node.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
-      });
+      node.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
     }
 
     function dragstarted(d) {
