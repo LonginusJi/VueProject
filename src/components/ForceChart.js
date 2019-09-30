5793e421ee08aa5c365af2ab0f539aabf0640be9
yijunji <jiyijun@outlook.com>
2019-09-30 11:54:24 +0800

    Separate jscode from ForceChart.vue

---
+++ src/components/ForceChart.js
@@ -0,0 +1,117 @@
+import * as d3 from "d3";
+
+export default class ForceChart {
+    constructor(graph, width, height) {
+        this.svg = d3.select("#ForceChart")
+            .attr("width", width)
+            .attr("height", height);
+        this.width = width;
+        this.height = height;
+
+        this.initData(graph);
+        this.initSimulation();
+        this.initDrag();
+        this.plotNode();
+        this.plotLink();
+        this.plotText();
+    }
+
+    initData(graph) {
+        this.graph = graph;
+        this.links = this.graph.links;
+        this.nodes = this.graph.nodes;
+    }
+
+    initSimulation() {
+        this.simulation = d3
+            .forceSimulation()
+            .force("link", d3.forceLink().id(d => d.id))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(this.width / 2, this.height / 2));
+    }
+
+    initDrag() {
+        let simulation = this.simulation
+
+        function dragstarted(d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        function dragged(d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        }
+
+        function dragended(d) {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+
+        this.drag = d3
+            .drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended)
+    }
+
+    plotNode() {
+        let color = d3.scaleOrdinal(d3.schemeSet1);
+        this.node = this.svg
+            .append("g")
+            .attr("class", "nodes")
+            .selectAll("g")
+            .data(this.nodes)
+            .join("g");
+
+        this.node
+            .append("circle")
+            .attr("r", 5)
+            .attr("fill", d => color(d.group))
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+            .call(this.drag);
+
+        this.simulation.nodes(this.nodes).on("tick", this.ticked.bind(this));
+        this.simulation
+            .force("link")
+            .links(this.links)
+            .distance(d => d.value * 20);
+    }
+
+    plotLink() {
+        this.link = this.svg
+            .append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(this.links)
+            .join("line")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            // .attr('opacity',0)
+            .attr("stroke-width", d => Math.sqrt(d.value));
+    }
+
+    plotText() {
+        this.node
+            .append("text")
+            .text(d => d.id)
+            .attr("x", 6)
+            .attr("y", 3)
+            .style("font-size", 12)
+            .style("font-family", "Times New Roman");
+        this.node.append("title").text(d => d.id);
+    }
+
+    ticked() {
+        this.link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+        this.node.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+    }
+}
--- src/components/ForceChart.vue
+++ src/components/ForceChart.vue
@@ -1,103 +1,114 @@
 <template>
   <div class="chart">
-    <svg id="ForceChart" width="760" height="600" />
+    <svg id="ForceChart" />
   </div>
 </template>
 
 <script>
-import * as d3 from "d3";
+import ForceChart from "./ForceChart";
 export default {
   mounted() {
-    var svg = d3.select("#ForceChart"),
-      width = +svg.attr("width"),
-      height = +svg.attr("height");
-
-    var color = d3.scaleOrdinal(d3.schemeSet1);
-
-    var simulation = d3
-      .forceSimulation()
-      .force("link", d3.forceLink().id(d => d.id))
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2));
-
-    const graph = require("../data/miserables.json");
-
-    var link = svg
-      .append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(graph.links)
-      .join("line")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-      // .attr('opacity',0)
-      .attr("stroke-width", d => Math.sqrt(d.value));
-
-    var node = svg
-      .append("g")
-      .attr("class", "nodes")
-      .selectAll("g")
-      .data(graph.nodes)
-      .join("g");
-
-    node
-      .append("circle")
-      .attr("r", 5)
-      .attr("fill", d => color(d.group))
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-      .call(
-        d3
-          .drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended)
-      );
-
-    node
-      .append("text")
-      .text(d => d.id)
-      .attr("x", 6)
-      .attr("y", 3)
-      .style("font-size", 12)
-      .style("font-family", "Times New Roman");
-
-    node.append("title").text(d => d.id);
-
-    simulation.nodes(graph.nodes).on("tick", ticked);
-
-    simulation
-      .force("link")
-      .links(graph.links)
-      .distance(d => d.value * 20);
-
-    function ticked() {
-      link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-
-      node.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
-    }
-
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
+    this.initForceChart();
+  },
+  methods: {
+    initForceChart() {
+      let width = 760;
+      let height = 600;
+      let graph = require("../data/miserables.json");
+      new ForceChart(graph, width, height);
     }
   }
+  // mounted() {
+  //   var svg = d3.select("#ForceChart"),
+  //     width = +svg.attr("width"),
+  //     height = +svg.attr("height");
+
+  //   var color = d3.scaleOrdinal(d3.schemeSet1);
+
+  //   var simulation = d3
+  //     .forceSimulation()
+  //     .force("link", d3.forceLink().id(d => d.id))
+  //     .force("charge", d3.forceManyBody())
+  //     .force("center", d3.forceCenter(width / 2, height / 2));
+
+  //   const graph = require("../data/miserables.json");
+
+  //   var link = svg
+  //     .append("g")
+  //     .attr("class", "links")
+  //     .selectAll("line")
+  //     .data(graph.links)
+  //     .join("line")
+  //     .attr("stroke", "#999")
+  //     .attr("stroke-opacity", 0.6)
+  //     // .attr('opacity',0)
+  //     .attr("stroke-width", d => Math.sqrt(d.value));
+
+  //   var node = svg
+  //     .append("g")
+  //     .attr("class", "nodes")
+  //     .selectAll("g")
+  //     .data(graph.nodes)
+  //     .join("g");
+
+  //   node
+  //     .append("circle")
+  //     .attr("r", 5)
+  //     .attr("fill", d => color(d.group))
+  //     .attr("stroke", "#fff")
+  //     .attr("stroke-width", 1.5)
+  //     .call(
+  //       d3
+  //         .drag()
+  //         .on("start", dragstarted)
+  //         .on("drag", dragged)
+  //         .on("end", dragended)
+  //     );
+
+  //   node
+  //     .append("text")
+  //     .text(d => d.id)
+  //     .attr("x", 6)
+  //     .attr("y", 3)
+  //     .style("font-size", 12)
+  //     .style("font-family", "Times New Roman");
+
+  //   node.append("title").text(d => d.id);
+
+  //   simulation.nodes(graph.nodes).on("tick", ticked);
+
+  //   simulation
+  //     .force("link")
+  //     .links(graph.links)
+  //     .distance(d => d.value * 20);
+
+  //   function ticked() {
+  //     link
+  //       .attr("x1", d => d.source.x)
+  //       .attr("y1", d => d.source.y)
+  //       .attr("x2", d => d.target.x)
+  //       .attr("y2", d => d.target.y);
+
+  //     node.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+  //   }
+
+  //   function dragstarted(d) {
+  //     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  //     d.fx = d.x;
+  //     d.fy = d.y;
+  //   }
+
+  //   function dragged(d) {
+  //     d.fx = d3.event.x;
+  //     d.fy = d3.event.y;
+  //   }
+
+  //   function dragended(d) {
+  //     if (!d3.event.active) simulation.alphaTarget(0);
+  //     d.fx = null;
+  //     d.fy = null;
+  //   }
+  // }
 };
 </script>
 
