297fd6505d1aa6f752899922b0121c81387d2257
yijunji <jiyijun@outlook.com>
2019-10-09 16:44:24 +0800

    Add background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,34 @@
 <template>
-  <v-card max-width='500' class='mx-auto' id='logIn'>
-    <v-card-text>
-      <v-text-field label='Username' type='text' v-model='username'></v-text-field>
-      <v-text-field label='Password' type='password' v-model='password'></v-text-field>
-      <span class='caption grey--text text--darken-1'>Please enter a password for your account</span>
-    </v-card-text>
-    <v-btn @click='logIn'>Log In</v-btn>
-    <v-alert type='info' v-model='show' dismissible>You can add '/Chart' after the local address</v-alert>
-  </v-card>
+  <v-parallax
+    dark
+    style="height:700px; max-width:100%; max-height:100%"
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-row align="center" justify="center">
+      <v-card max-width="500" class="mx-auto" id="logIn">
+        <v-card-text>
+          <v-text-field label="Username" type="text" v-model="username"></v-text-field>
+          <v-text-field label="Password" type="password" v-model="password"></v-text-field>
+          <span class="caption grey--text text--darken-1">Please enter a password for your account</span>
+        </v-card-text>
+        <v-btn @click="logIn">Log In</v-btn>
+        <v-alert type="info" v-model="show" dismissible>You can add '/Chart' after the local address</v-alert>
+      </v-card>
+    </v-row>
+  </v-parallax>
 </template>
 
 <script>
 export default {
   data() {
-    return { username: '', password: '', show: '' };
+    return { username: "", password: "", show: "" };
   },
   methods: {
     logIn() {
       a: if (this.password.length >= 5) {
         for (var i = 0; i < this.password.length; i++) {
-          if (this.password.slice(i, i + 5) == 'magic') {
-            this.$router.push({ path: '/Chart' });
+          if (this.password.slice(i, i + 5) == "magic") {
+            this.$router.push({ path: "/Chart" });
             break a;
           }
         }
@@ -34,6 +42,12 @@ export default {
 </script>
 
 <style>
+.v-parallax {
+  overflow: visible;
+}
+.v-parallax__image-container {
+  contain: none;
+}
 #logIn {
   margin: 10%;
   text-align: center;
